6de1c55f930d2393f40b44a374184a23c31ca283
ttitto <ttitto_prog@vfemail.net>
2015-05-18 23:19:06 +0300

    sample data seede

---
+++ Peek.Common/PeekConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peek.Common
+{
+    public  static class PeekConstants
+    {
+        public const string AdminRole = "Admin";
+
+        public const string AdminUsername = "admin";
+    }
+}
--- Peek.Data/Migrations/Configuration.cs
+++ Peek.Data/Migrations/Configuration.cs
@@ -6,29 +6,115 @@ namespace Peek.Data.Migrations
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Peek.Models;
+    using Peek.Common;
+    using System.Collections.Generic;
 
     public sealed class Configuration : DbMigrationsConfiguration<PeekDbContext>
     {
+        private IRandomGenerator random;
+        private UserManager<User> userManager;
+
         public Configuration()
         {
             this.AutomaticMigrationsEnabled = true;
             this.AutomaticMigrationDataLossAllowed = true;
+            this.random = new RandomGenerator();
         }
 
         protected override void Seed(PeekDbContext context)
         {
-            if (!context.Roles.Any() && !context.Users.Any())
-            {
-                var adminRole = new IdentityRole("Administrator");
-                context.Roles.Add(adminRole);
+            this.userManager = new UserManager<User>(new UserStore<User>(context));
 
-                var store = new UserStore<User>(context);
-                var manager = new UserManager<User>(store);
-                var admin = new User { UserName = "admin", Email = "admin@peek.com" };
-                manager.Create(admin, "peekadmin");
-                manager.AddToRole(admin.Id, "Administrator");
+            this.SeedUsers(context);
+            this.SeedCategories(context);
+            this.SeedProductsWithComments(context);
+            this.SeedOrders(context);
+        }
+
+        private void SeedOrders(PeekDbContext context)
+        {
+            if (context.Orders.Any())
+            {
+                return;
             }
 
+            var users = context.Users.ToList();
+            var allProducts = context.Products.ToList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var products = new List<Product>();
+
+                foreach (var product in products)
+                {
+                    if (this.random.RandomNumber(0, 100) % 2 == 0)
+                    {
+                        products.Add(product);
+                    }
+                }
+
+                var order = new Order
+                {
+                    Status = (OrderStatus)Enum.Parse(typeof(OrderStatus), this.random.RandomNumber(0, 2).ToString()),
+                    User = users[this.random.RandomNumber(0, users.Count() - 1)],
+                    Products = products,
+                    CreatedOn = DateTime.Now - new TimeSpan(this.random.RandomNumber(1, 500), 0, 0)
+                };
+
+                context.Orders.Add(order);
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedProductsWithComments(PeekDbContext context)
+        {
+            if (context.Products.Any())
+            {
+                return;
+            }
+
+            var categories = context.Categories.ToList();
+            var users = context.Users.ToList();
+
+            for (int i = 0; i < 30; i++)
+            {
+                var product = new Product
+                    {
+                        Name = this.random.RandomString(10, 50),
+                        Description = this.random.RandomString(100, 1000),
+                        Price = this.random.RandomNumber(1, 120),
+                        Category = categories[this.random.RandomNumber(1, 100) % categories.Count],
+                        CreatedOn = DateTime.Now - new TimeSpan(this.random.RandomNumber(1, 500), 0, 0),
+                        CreatedUser = users[this.random.RandomNumber(1, users.Count() - 1)],
+                        InStock = this.random.RandomNumber(0, 10) % 2 == 0
+                    };
+
+                var comments = new List<Comment>();
+                for (int k = 0; k < 40; k++)
+                {
+                    var comment = new Comment
+                    {
+                        PostedAt = DateTime.Now,
+                        Product = product,
+                        Text = this.random.RandomString(20, 1000),
+                        User = users[this.random.RandomNumber(0, users.Count - 1)]
+                    };
+
+                    context.Comments.Add(comment);
+
+                    if (this.random.RandomNumber(5, 10) > 7)
+                    {
+                        break;
+                    }
+                }
+
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedCategories(PeekDbContext context)
+        {
             if (!context.Categories.Any())
             {
                 context.Categories.Add(new Category
@@ -52,7 +138,7 @@ namespace Peek.Data.Migrations
                     Name = "Cars",
                     IsActive = true,
                     CreatedOn = DateTime.Now,
-                    CreatedUser = context.Users.First()
+                    CreatedUser = context.Users.ToList()[this.random.RandomNumber(0, context.Users.Count() - 1)]
                 });
 
                 context.Categories.Add(new Category
@@ -62,6 +148,42 @@ namespace Peek.Data.Migrations
                     CreatedOn = DateTime.Now,
                     CreatedUser = context.Users.First()
                 });
+
+                context.Categories.Add(new Category
+                {
+                    Name = this.random.RandomString(150, 300),
+                    IsActive = true,
+                    CreatedOn = DateTime.Now,
+                    CreatedUser = context.Users.ToList()[this.random.RandomNumber(0, context.Users.Count() - 1)]
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        private void SeedUsers(PeekDbContext context)
+        {
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                var user = new User
+                {
+                    Email = string.Format("{0}@{1}.com", this.random.RandomString(6, 16), this.random.RandomString(6, 16)),
+                    UserName = this.random.RandomString(6, 16)
+                };
+
+                this.userManager.Create(user, "123456");
+            }
+
+            if (!context.Roles.Any() && !context.Users.Any())
+            {
+                var admin = new User { UserName = PeekConstants.AdminUsername, Email = "admin@peek.com" };
+                this.userManager.Create(admin, "peekadmin");
+                this.userManager.AddToRole(admin.Id, PeekConstants.AdminRole);
             }
         }
     }
