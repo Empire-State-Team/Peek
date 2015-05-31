01a9ec651e5fa5ab153cde986181b747a5c9a88b
tr00per92 <zvezdo@mail.bg>
2015-05-31 12:54:17 +0300

    removed some references

--- Peek.Web/App_Start/Startup.Auth.cs
+++ Peek.Web/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@
     using Microsoft.AspNet.Identity.Owin;
     using Microsoft.Owin;
     using Microsoft.Owin.Security.Cookies;
-    using Microsoft.Owin.Security.Google;
     using Owin;
     using Peek.Data;
     using Peek.Models;
