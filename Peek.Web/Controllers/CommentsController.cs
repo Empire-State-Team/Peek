fa8568e53919ddfab2f1473d8e77728933def3d4
ttitto <ttitto_prog@vfemail.net>
2015-06-04 09:18:24 +0300

    try to open form for comment creation

---
+++ Peek.Web/Controllers/CommentsController.cs
@@ -0,0 +1,19 @@
+﻿namespace Peek.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Peek.Data.UnitOfWork;
+
+    public class CommentsController : BaseController
+    {
+        public CommentsController(IPeekData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult CreateForProductId(int id)
+        {
+            return PartialView("_CreateCommentForProduct");
+        }
+    }
+}
